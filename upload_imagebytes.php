72f0fabfa2d52b2897cef898afec12c659c495f1
Humberto Castañeda Garza <humberto.castaneda@definityfirst.com>
2017-03-01 15:38:48 -0500

    Changed saving directory to this format  YYYY/YYYYMM/YYYYMMDD

--- upload_imagebytes.php
+++ upload_imagebytes.php
@@ -1,7 +1,13 @@
 <?php
 include 'SQLHelper.php';
 $destFolder = dirname ( __FILE__ );
-$uploadfile = $destFolder . $_POST ["ruta"];
+
+$ruta = substr($_POST ["ruta"], 2,14);
+$archivo = substr($_POST ["ruta"], -39);
+
+$ruta = substr($ruta, 0, 10) .'/'. substr($ruta, 6, 6).'/'. substr($ruta, 6);
+
+$uploadfile = $destFolder . '/'. $ruta .'/'. $archivo;
 
 $subido = false;
 $grabarencadena = false;
@@ -9,12 +15,13 @@ $cadena = $_POST ["cadena"];
 $resultado = 0;
 $errorcod = 0;
 
-$carpeta = $_POST ["carpeta"];
+$carpeta = '/'. $ruta .'/';
 
 $uploadfile = str_replace ( " ", "_", $uploadfile );
 
 if (! file_exists ( $destFolder . $carpeta )) {
-	mkdir ( $destFolder . $carpeta, 0777, true );
+	
+	mkdir( $destFolder . $carpeta, 0777, true );
 }
 
 if (file_exists ( $uploadfile )) {
@@ -36,7 +43,7 @@ if ($resultado == 0) {
 	
 	if ($fh) {
 		$helper = new SQLHelper();
-		$helper->saveImage(substr($_POST ["ruta"], 2,14), substr($_POST ["ruta"], -39));
+		$helper->saveImage($ruta,$archivo );
 		echo "0";
 	} else if ($subido) {
 		echo "No se pudo subir el archivo " . $error;
